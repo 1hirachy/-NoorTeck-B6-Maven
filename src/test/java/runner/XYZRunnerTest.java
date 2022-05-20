b90dabeb3b5306a84b9f01646a33db304e66e497
hira_ <hira_@Md-Chowdhury>
2022-05-19 20:44:10 -0500

    pushing my project

---
+++ src/test/java/runner/XYZRunnerTest.java
@@ -0,0 +1,25 @@
+package runner;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+public class XYZRunnerTest {
+	
+@RunWith(Cucumber.class)
+
+@CucumberOptions(
+
+			features = "src/test/resources/OrangeHRM/MercuryRegister.feature", 
+			glue = {"com.noorteck.qa.steps" }, 
+			plugin = { "pretty","html:target/cucumber-report.html" }, 
+			monochrome = true
+
+	)
+
+	public class Runner {
+
+	}
+
+}
---
+++ src/test/resources/orangehrm/SmokeTest.feature
@@ -0,0 +1,4 @@
+Feature: Description if feature/functionality we will be testing
+
+  Scenario: Verify user able to login successfully
+    Given User navigates to login page
